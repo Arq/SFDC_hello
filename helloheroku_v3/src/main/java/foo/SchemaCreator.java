3e9c25380868ef7c07e4d6bfbfd15b7035701f03
ARQ <arkadiusz.grzesiak@gmail.com>
2015-01-22 00:25:11 +0100

    .

---
+++ helloheroku_v3/src/main/java/foo/SchemaCreator.java
@@ -0,0 +1,9 @@
+package foo;
+import java.sql.SQLException;
+
+public class SchemaCreator {
+	public static void main(String[] args) throws SQLException {
+		TickDAO tickDAO = new TickDAO();
+		TickDAO.createTable();
+	}
+}
---
+++ helloheroku_v3/src/main/java/foo/Ticker.java
@@ -0,0 +1,16 @@
+package foo;
+public class Ticker {
+public static void main(String[] args) {
+	TickDAO tickDAO = new TickDAO();
+		try {
+			while (true) {
+				tickDAO.insertTick();
+				System.out.println("tick");
+				Thread.sleep(1000);
+			}
+		}
+		catch (Exception e) {
+			// exception handling omitted for brevity
+		}
+	}
+}
